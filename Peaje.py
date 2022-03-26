f094a1c8f3b83424047f4a971b9459fd92d1a36f
NextCodigos <nebulanemesis376@gmail.com>
2022-03-26 01:18:42 +0100

    subiendo el 29 archivo

---
+++ Peaje.py
@@ -0,0 +1,86 @@
+"""
+Crear un programa que permita el ingreso del
+nombre y la edad yo he cambiado a varidades de
+nisperos ALGAR y kilos
+
+CALIDAD(NISPERO ALGAR):
+(GGG EXTRA),(GG(EXTRA),GG(I rozado),(G EXTRA),(G I rozado)
+(M I),(P II),(SEPARADA II),(F.P :)
+
+TOTAL KILOS ALGAR calcular
+
+
+"""
+"""
+calidad= input("Ingrese calidad de nispero algar GGG EXTRA: ")
+kilos=input("Ingresa kilos de nisperos: ")
+print("Su calidad es:")
+print(calidad)
+print("Los kilos son:")
+print(kilos)
+
+calidad1= input("Ingrese calidad de nispero algar GG EXTRA: ")
+kilos1=input("Ingresa kilos de GG EXTRA: ")
+print("Su calidad es:")
+print(calidad1)
+print("Los kilos son:")
+print(kilos1)
+"""
+
+""""(GGG EXTRA),(GG(EXTRA),GG(I rozado),(G EXTRA),(G I rozado)
+(M I),(P II),(SEPARADA II),(F.P :)"""
+
+"""
+Cre un programa que pida dos  numeros al usuario y 
+muestre la suma: Yo he hecho que pida dos calidades de nisperos de kilos y los sume
+,de GG extra y GG ROzado ,etc
+
+falta pedirle ingrese la suma de todas las variadades y las sume y las muestre por 
+pantalla la suma total
+
+num1=int(input("Ingrese el primer numero: "))
+num2=int(input("Ingrese el segundo numero: "))
+total= num1 + num2
+print("El resultado es:")
+print(total)
+
+"""
+
+""""(GGG EXTRA),(GG(EXTRA),GG(I rozado),(G EXTRA),(G I rozado)
+(M I pequeño),(P II segunda),("SEPARADA II caja negra destrio no se suman van aparte"),
+(F.P fondo perdido descuentan kilos:)"""
+
+num0=int(input("Ingrese los kilos de GGGg EXTRA: "))
+print("Los kilos de algar de GGG extra son:")
+total0=num0
+print(total0)
+
+num1=int(input("Ingrese los kilos de GG EXTRA: "))
+num2=int(input("Ingrese los kilos de GG I Rozado: "))
+total1= num1 + num2
+print("Los kilos de algar de GG extra y rozado son:")
+print(total1)
+
+num3=int(input("Ingrese los kilos de G EXTRA: "))
+num4=int(input("Ingrese los kilos de G I rozado: "))
+total2= num3 + num4
+print("Los kilos algar de G EXTRA y G I rozado son:")
+print(total2)
+
+num5=int(input("Ingrese los kilos de M I pequeño: "))
+print("Los kilos de algar de M I pequeño son:")
+total5=num5
+print(total5)
+
+num6=int(input("Ingrese los kilos de P II segunda: "))
+print("Los kilos de algar de P II segunda son:")
+total6=num6
+print(total6)
+
+
+print("la suma de todos los kilos de algar ggg gg y g M I pequeño P II segunda son:" , (total0 +total1 + total2 + total5 + total6),"kgs")
+
+num7=int(input("Ingrese los kilos dese SEPARADA II caja negra destrio no se suman van aparte: "))
+total7=num7
+print("Los kilos dese SEPARADA II caja negra destrio no se suman van aparte son: " , (total7),"kgs")
+print("la suma de todos los kilos de algar ggg gg y g M I pequeño P II segunda son:" , (total0 +total1 + total2 + total5 + total6),"kgs")
